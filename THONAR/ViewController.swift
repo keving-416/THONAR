9af9e8a4444cc29b0b418ff1100f151078491442
kevingardner <kjg5556@psu.edu>
2018-12-15 21:02:33 -0800

    - Add page curl animation - Make manu button more clear

--- THONAR/ViewController.swift
+++ THONAR/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     var mode: String = "Default"
     @IBOutlet weak var gameButton: MenuButton!
     @IBOutlet weak var storybookButton: MenuButton!
+    @IBOutlet weak var menuButton: UIButton!
     
     @IBOutlet var sceneView: ARSCNView!
     @IBOutlet var menuView: UIView!
@@ -72,6 +73,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Set text of modeLabel
         modeLabel?.text = mode
+        
+        menuButton.layer.cornerRadius = menuButton.frame.width/2
     }
     
     func setButtonModes() {
@@ -197,6 +200,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         menuView.alpha = 0
         
         UIView.animate(withDuration: 0.4) {
+            self.menuButton.isHidden = true
             self.visualEffectView.isHidden = false
             self.visualEffectView.effect = self.effect
             self.menuView.alpha = 1
@@ -205,15 +209,57 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
     
     func animateMenuOut() {
+        /*
         UIView.animate(withDuration: 0.3, animations: {
             self.menuView.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
             self.menuView.alpha = 0
             
             self.visualEffectView.effect = nil
+            self.menuButton.isHidden = false
             
         }) { (success:Bool) in
             self.menuView.removeFromSuperview()
         }
+         */
+        //perform(#selector(flip), with: nil)
+        UIView.animate(withDuration: 1.0, animations: {
+            let animation = CATransition()
+            animation.duration = 1.2
+            animation.startProgress = 0.0
+            animation.endProgress = 1.2
+            animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
+            animation.type = CATransitionType(rawValue: "pageCurl")
+            animation.subtype = CATransitionSubtype(rawValue: "fromRight")
+            animation.isRemovedOnCompletion = false
+            animation.fillMode = CAMediaTimingFillMode(rawValue: "extended")
+            self.visualEffectView.layer.add(animation, forKey: "pageFlipAnimation")
+            //var tempView = UIView()
+            //tempView.backgroundColor = UIColor.blue
+            //self.visualEffectView.addSubview(tempView)
+        }) { (success:Bool) in
+            self.menuView.alpha = 0
+            self.visualEffectView.effect = nil
+            self.menuButton.isHidden = false
+            self.menuView.removeFromSuperview()
+        }
+    }
+    
+    @objc func flip() {
+        let transitionOptions: UIView.AnimationOptions = [.transitionCurlUp, .showHideTransitionViews]
+        
+        UIView.transition(with: menuView, duration: 1.0, options: transitionOptions, animations: {
+            self.menuView.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
+            self.menuView.alpha = 0
+            
+            self.visualEffectView.effect = nil
+            self.menuButton.isHidden = false
+            self.menuView.isHidden = true
+        })
+        
+        UIView.transition(with: sceneView, duration: 1.0, options: transitionOptions, animations: {
+            self.menuView.removeFromSuperview()
+            self.sceneView.isHidden = false
+        })
     }
 }
 
